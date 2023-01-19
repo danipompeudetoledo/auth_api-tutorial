bb2abba3dda8db61f23e6de3b370270eb19ca2a9
Danilo Santos <danilosantos.itech@gmail.com>
2023-01-18 23:45:30 -0300

    add positive and negative test case

--- cypress/integration/specs/auth.spec.js
+++ cypress/integration/specs/auth.spec.js
@@ -1,7 +1,27 @@
 it ('returns 200 when we hit / Register ',()=>{
-    cy.request('POST', 'http://localhost:3000/api/user/register')
+    let body = {
+        name:'TestName',
+        email: 'foo@bar.com',
+        password:'Test0987'
+
+    }
+    cy.request('POST', 'http://localhost:3000/api/user/register', body)
         .then((response) => {
             expect(response.status).to.eq(200);
+
+
+    })
+})
+
+it ('returns 400 when we hit / Register  with no body ',()=>{
+
+    cy.request({
+
+        method:'POST',
+        url: 'http://localhost:3000/api/user/register',
+        failOnStatusCode:false
+    }).then((response) => {
+            expect(response.status).to.eq(400);
 
 
     })
